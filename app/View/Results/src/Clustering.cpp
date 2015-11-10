0e989de581b1cc9580b1a1c4ea460d4295fb207c
richlvnsn <richlvnsn@gmail.com>
2015-11-09 20:07:23 -0500

    Commented Clustering.cpp and Clustering.h

--- app/View/Results/src/Clustering.cpp
+++ app/View/Results/src/Clustering.cpp
@@ -4,7 +4,14 @@
 //
 //  Created by Thomas Borgia on 6/13/15.
 //
-//
+// Name: Richard Levenson
+// Course: CSC 415
+// Semester: Fall 2015
+// Instructor: Dr. Pulimood
+// Project Name: Pollution Prediction
+// Description: Clustering class which maintains a vector of clusters and handles writing the cluster information to files.
+// Filename: Clustering.cpp
+// Last Modified On: 11/9/15 by Richard Levenson
 
 #include "Clustering.h"
 
@@ -12,9 +19,12 @@ using namespace std;
 
 string path2 = "/var/www/html/SOAP/app/View/Results/Clusters/";
 
+//----------------------------------------------------------------------------------------------------------
+// Clustering object constructor.
+//----------------------------------------------------------------------------------------------------------
 Clustering::Clustering(vector<Facility> init)
 {
-	D = init;
+	D = init; // A list of all of the facilities available for clustering
 }
 
 //----------------------------------------------------------------------------------------------------------
@@ -23,22 +33,26 @@ Clustering::Clustering(vector<Facility> init)
 void Clustering::DBSCAN(float eps, int MinPts)
 {
 
-	vector<Facility*> NeighborPts;
+	vector<Facility*> NeighborPts;  //Holds a vector of pointers to neighboring points
 
-	C = 0;
-
+	C = 0; // the initial number of clusters is 0
+	/* Loops for all of the facilities */
 	for(int i=0; i<D.size(); i++)
 	{
+		/* Enter the if statement only if the facility has not yet been visited */
 		if(!D.at(i).getVisited())
 		{
-			NeighborPts = regionQuery(D.at(i), eps);
-			D.at(i).setVisited(true);
+			/* Determine if the facility is nearby any other facilities */
+			NeighborPts = regionQuery(D.at(i), eps); // hold the vector of pointers to nighboring facilities in NeighborPts
+			D.at(i).setVisited(true); // set that the facility has been visited
+			/* Check if there are at least MinPts facilities in this vector of neighboring facilities */
 			if(NeighborPts.size() < MinPts)
-				noise.push_back(D.at(i));
+				noise.push_back(D.at(i));  // If less than MinPts facilities, add it to the list of noise
 			else
 			{
+				/* If more than MinPts facilities, create a new cluster */
 				expandCluster(D.at(i), NeighborPts, C, eps, MinPts);
-				C++;
+				C++; // increment the number of clusters
 			}
 		}
 	}
@@ -51,35 +65,40 @@ void Clustering::expandCluster(Facility P, vector<Facility*> NeighborPts, int C,
 {
 	vector<Facility> init;
 	cluster.push_back(init);
-
+	/* Loop for all of the pointers in the vector of Facility pointers */
 	for(int i=0; i<(NeighborPts.size()); i++)
 	{
-		vector<Facility*> Neighbor_Pts;
-
+		vector<Facility*> Neighbor_Pts; // holds neighboring points
+		/* Enter the if statement only if the facility has not yet been visited */
 		if(!NeighborPts.at(i)->getVisited())
 		{
-			NeighborPts.at(i)->setVisited(true);
+			NeighborPts.at(i)->setVisited(true); // set that the facility has been visited
+			/* Determine if the facility pointed to by the pointer is nearby any other facilities */
 			Neighbor_Pts = regionQuery(*NeighborPts.at(i), eps);
+			/* Check if there are at least MinPts facilities in this vector of neighboring facilities */
 			if(Neighbor_Pts.size() >= MinPts)
 			{
+				/* Loop for all neighboring facilities in local variable Neighbor_Pts */
 				for(int j=0; j<Neighbor_Pts.size(); j++)
 				{
 					bool dupl=false;
-
+					/* Loop for all neighboring facilities in parameter NeighborPts */
 					for(int k=0; k<NeighborPts.size(); k++){
+						/* Identify if the facility is a duplicate */
 						if(Neighbor_Pts.at(j)->getID() == NeighborPts.at(k)->getID())
 							dupl=true;
 					}
+					/* If the facility is not a duplicate, add it to the parameter NeighborPts */
 					if(!dupl)
 						NeighborPts.push_back(Neighbor_Pts.at(j));
 				}
 			}
 		}
-
+		/* If the facility has not yet been clustered, add it to the cluster */
 		if (!NeighborPts.at(i)->getClustered())
 		{
-			cluster.at(C).push_back(*NeighborPts.at(i));
-			NeighborPts.at(i)->setClustered(true);
+			cluster.at(C).push_back(*NeighborPts.at(i)); // add to the cluster
+			NeighborPts.at(i)->setClustered(true); // set that the facility has been clustered
 		}
 	}
 }
@@ -89,37 +108,45 @@ void Clustering::expandCluster(Facility P, vector<Facility*> NeighborPts, int C,
 //----------------------------------------------------------------------------------------------------------
 vector<Facility*> Clustering::regionQuery(Facility P, float eps)
 {
-	float dist;
-	vector<Facility*> localPts;
+	//eps is the longitude/latitude difference threshold for determining if two points are close enough to be considered in the same cluster
+	float dist; //distance between two points
+	vector<Facility*> localPts; //Holds a vector of pointers to neighboring points
+	/* Loops for all of the facilities */
 	for(int i=0; i<D.size(); i++)
 	{
+		/* Calculates the distance between the current facility and the facility P */
 		dist = sqrt(pow((P.getLat() - D.at(i).getLat()), 2) + pow((P.getLon() - D.at(i).getLon()), 2));
 		if(dist <= eps && dist != 0.0)
 		{
+			/* If P is within distance eps of the facility, add a pointer to P to the local vector of Facility pointers localPts  */
 			localPts.push_back(&D.at(i));
 		}
 	}
-	return localPts;
+	return localPts; // Returns the vector of pointers to neighboring points
 }
 
+//----------------------------------------------------------------------------------------------------------
+// Prints locaton information for each cluster to the "index.csv" file
+//----------------------------------------------------------------------------------------------------------
 void Clustering::printIndex()
 {
 
 	ofstream outputFile;
-	string outputFileName = path2 + "index.csv";
-	outputFile.open(outputFileName.c_str());
-
+	string outputFileName = path2 + "index.csv"; // Printing to the "index.csv" file
+	outputFile.open(outputFileName.c_str()); // Opens the output file
+	/* Loops for the total number of clusters */
 	for(int i=0; i<C; i++)
 	{
-
+		/* Initial minimum and maximum longitudes and latitudes for the cluster */
 		double minLat = 90;
 		double minLon = 180;
 		double maxLat = -90;
 		double maxLon = -180;
 
-
+		/* Loops for the number of facilities in this cluster */
 		for(int j=0; j<cluster.at(i).size(); j++)
 		{
+			/* Updates the minimum and maximum longitudes and latitudes for the cluster based on the locations of the facilities */
 			if(cluster.at(i).at(j).getLon() <= minLon)
 				minLon = cluster.at(i).at(j).getLon();
 			if(cluster.at(i).at(j).getLon() >= maxLon)
@@ -129,7 +156,7 @@ void Clustering::printIndex()
 			if(cluster.at(i).at(j).getLat() >= maxLat)
 				maxLat = cluster.at(i).at(j).getLat();
 		}
-
+		/* Prints in CSV format the cluster number, minimum latitude, maximum latitude, minimum longitude, and maximum longitude for this cluster */
 		outputFile << "cluster-" << to_string(i) << "," << to_string(minLat) << "," << to_string(maxLat) << "," << to_string(minLon) << "," << to_string(maxLon) << endl;
 	}
 	outputFile.close();
@@ -140,14 +167,16 @@ void Clustering::printIndex()
 //----------------------------------------------------------------------------------------------------------
 void Clustering::printLocationInfo()
 {
+	/* Loops for the total number of clusters */
 	for(int i=0; i<C; i++)
 	{
 		ofstream outputFile;
-		string outputFileName = path2 + "Locations/cluster-" + to_string(i) + ".csv";
-		outputFile.open(outputFileName.c_str());
-
+		string outputFileName = path2 + "Locations/cluster-" + to_string(i) + ".csv"; // Creates a different output file for each cluster in the Locations directory
+		outputFile.open(outputFileName.c_str()); // Opens the output file for this cluster
+		/* Loops for the number of facilities in this cluster */
 		for(int j=0; j<cluster.at(i).size(); j++)
 		{
+			/* Prints in CSV format the facility ID, facility name, latitude, and longitude for this facility*/
 			outputFile << cluster.at(i).at(j).getID() << "," << cluster.at(i).at(j).getName() << "," << cluster.at(i).at(j).getLat() << "," << cluster.at(i).at(j).getLon() << endl;
 		}
 		outputFile.close();
@@ -159,16 +188,19 @@ void Clustering::printLocationInfo()
 //----------------------------------------------------------------------------------------------------------
 void Clustering::printContainsInfo()
 {
+	/* Loops for the total number of clusters */
 	for(int i=0; i<C; i++)
 	{
 		ofstream outputFile;
-		string outputFileName = path2 + "Contains/cluster-" + to_string(i) + ".csv";
-		outputFile.open(outputFileName.c_str());
-
+		string outputFileName = path2 + "Contains/cluster-" + to_string(i) + ".csv"; // Creates a different output file for each cluster in the Contains directory
+		outputFile.open(outputFileName.c_str()); // Opens the output file for this cluster
+		/* Loops for the number of facilities in this cluster */
 		for(int j=0; j<cluster.at(i).size(); j++)
 		{
+			/* Loops for the number of chemicals in this facility */
 			for(int k=0; k<cluster.at(i).at(j).getChemCount(); k++)
 			{
+				/* Prints in CSV format the facility ID, chemical ID,  chemical name, and chemical weight for this chemical*/
 				outputFile << cluster.at(i).at(j).getID() << "," << cluster.at(i).at(j).getChemIDAt(k) << "," << cluster.at(i).at(j).getChemNameAt(k) << "," << cluster.at(i).at(j).getChemTAAt(k) << endl;
 			}
 		}
--- app/View/Results/src/Clustering.h
+++ app/View/Results/src/Clustering.h
@@ -4,11 +4,19 @@
 //
 //  Created by Thomas Borgia on 6/13/15.
 //
-//
+// Name: Richard Levenson
+// Course: CSC 415
+// Semester: Fall 2015
+// Instructor: Dr. Pulimood
+// Project Name: Pollution Prediction
+// Description: D\Header file to define the Clustering class.
+// Filename: Clustering.h
+// Last Modified On: 11/9/15 by Richard Levenson
 
-#ifndef ____Clustering__
-#define ____Clustering__
-
+/* Preprocessor Directives */
+#ifndef ____Clustering__ //Run other preprocessor directives only if ____Clustering__ has not already been defined
+#define ____Clustering__ //Define the ___Clustering__ macro
+/* Include cpp header files */
 #include <stdio.h>
 #include <stdlib.h>
 #include <cmath>
@@ -17,21 +25,21 @@
 #include "Facility.h"
 
 #endif /* defined(____Clustering__) */
-
+/* Clutering class header file*/
 class Clustering
 {
 	private:
-		int C;
-		std::vector<Facility> D;
-		std::vector<std::vector<Facility> > cluster;
-		std::vector<Facility> noise;
+		int C; //number of clusters
+		std::vector<Facility> D; // vector of all of the facilities
+		std::vector<std::vector<Facility> > cluster; //vector of all of the clusters
+		std::vector<Facility> noise; //vector of facilities representing "noise"
 
 	public:
-		Clustering(std::vector<Facility>);
-		void DBSCAN(float, int);
-		void expandCluster(Facility, std::vector<Facility*>, int, float, int);
-		std::vector<Facility*> regionQuery(Facility, float);
-		void printIndex();
-		void printLocationInfo();
-		void printContainsInfo();
+		Clustering(std::vector<Facility>); // Clustering object constructor
+		void DBSCAN(float, int); // Density - based spatial clustering of applications with noise
+		void expandCluster(Facility, std::vector<Facility*>, int, float, int); // When point's neighborhood is too small, add to exisiting nearby cluster
+		std::vector<Facility*> regionQuery(Facility, float);// To check Euclidean distance between two points to determine clusters
+		void printIndex(); // Prints locaton information for each cluster to the "index.csv" file
+		void printLocationInfo(); // To create location data and output to file for Point Analysis Program
+		void printContainsInfo(); // To create location data and output to file for Point Analysis Program
 };
